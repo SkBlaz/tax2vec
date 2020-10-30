f3c8667644e299a604e590cede85d43ffec86eb7
CitizenIsErased <timi.sp@gmail.com>
2020-10-30 15:02:24 +0100

    Examples modified for Windows multiprocessing

--- examples/demo_classification_custom_features.py
+++ examples/demo_classification_custom_features.py
@@ -6,37 +6,40 @@ from tax2vec.preprocessing import *
 from tax2vec.models import *
 from sklearn.model_selection import train_test_split
 
+def run():
+    PAN_dataset = pd.read_csv("../datasets/PAN_2016_age_srna_en.tsv", sep="\t")
 
-PAN_dataset = pd.read_csv("../datasets/PAN_2016_age_srna_en.tsv", sep="\t")
+    ## generate learning examples
+    result_vector = []
+    num_splits = 1
 
-## generate learning examples
-result_vector = []
-num_splits = 1
+    ## do the stratified shufflesplit
+    for _ in range(num_splits):
 
-## do the stratified shufflesplit
-for _ in range(num_splits):
+        (train_x, test_x, train_y, test_y) = train_test_split(PAN_dataset['text'].values.tolist(), PAN_dataset['class'].values.tolist(), test_size=0.1)
 
-    (train_x, test_x, train_y, test_y) = train_test_split(PAN_dataset['text'].values.tolist(), PAN_dataset['class'].values.tolist(), test_size=0.1)
+        tax2vec_instance = t2v.tax2vec(max_features=50, targets=train_y, num_cpu=8, heuristic="closeness_centrality", class_names=PAN_dataset['age_group'].values.tolist())
+        semantic_features_train = tax2vec_instance.fit_transform(train_x)
 
-    tax2vec_instance = t2v.tax2vec(max_features=50, targets=train_y, num_cpu=8, heuristic="closeness_centrality", class_names=PAN_dataset['age_group'].values.tolist())
-    semantic_features_train = tax2vec_instance.fit_transform(train_x)
+        ## get test features
+        train_matrices_for_svm = []
+        test_matrices_for_svm = []
+        semantic_features_test = tax2vec_instance.transform(test_x)
 
-    ## get test features
-    train_matrices_for_svm = []
-    test_matrices_for_svm = []
-    semantic_features_test = tax2vec_instance.transform(test_x)
+        train_matrices_for_svm.append(semantic_features_train)
+        test_matrices_for_svm.append(semantic_features_test)
 
-    train_matrices_for_svm.append(semantic_features_train)
-    test_matrices_for_svm.append(semantic_features_test)
+        tfidf_word_train, tokenizer_2, _ = data_docs_to_matrix(train_x, mode="matrix_pan")
+        tfidf_word_test = tokenizer_2.transform(build_dataframe(test_x))
+        train_matrices_for_svm.append(tfidf_word_train)
+        test_matrices_for_svm.append(tfidf_word_test)
+        
+        ## stack features (sparse)
+        features_train = hstack(train_matrices_for_svm)
+        features_test = hstack(test_matrices_for_svm)
 
-    tfidf_word_train, tokenizer_2, _ = data_docs_to_matrix(train_x, mode="matrix_pan")
-    tfidf_word_test = tokenizer_2.transform(build_dataframe(test_x))
-    train_matrices_for_svm.append(tfidf_word_train)
-    test_matrices_for_svm.append(tfidf_word_test)
+        ## run the SVM, where C=50
+        tmp_result = linear_SVM(features_train, features_test, train_y, test_y, cparam=50)
     
-    ## stack features (sparse)
-    features_train = hstack(train_matrices_for_svm)
-    features_test = hstack(test_matrices_for_svm)
-
-    ## run the SVM, where C=50
-    tmp_result = linear_SVM(features_train, features_test, train_y, test_y, cparam=50)
+if __name__ == '__main__':
+    run()
--- examples/demo_explain_unsupervised.py
+++ examples/demo_explain_unsupervised.py
@@ -6,24 +6,27 @@ import tax2vec as t2v
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+def run():
+    PAN_dataset = pd.read_csv("../datasets/PAN_2016_age_srna_en.tsv", sep="\t")
 
-PAN_dataset = pd.read_csv("../datasets/PAN_2016_age_srna_en.tsv", sep="\t")
+    ## generate learning examples
+    result_vector = []
+    num_splits = 1
 
-## generate learning examples
-result_vector = []
-num_splits = 1
+    ## do the stratified shufflesplit
+    for _ in range(num_splits):
 
-## do the stratified shufflesplit
-for _ in range(num_splits):
+        (train_x, test_x, train_y, test_y) = train_test_split(PAN_dataset['text'].values.tolist(), PAN_dataset['class'].values.tolist(), test_size=0.1)
 
-    (train_x, test_x, train_y, test_y) = train_test_split(PAN_dataset['text'].values.tolist(), PAN_dataset['class'].values.tolist(), test_size=0.1)
+        ## inititalize tax2vec object adn fit and transform training data
+        tax2vec_instance = t2v.tax2vec(max_features=50, num_cpu=8, heuristic="pagerank", mode="index_word")
+        semantic_features_train = tax2vec_instance.fit_transform(train_x)
 
-    ## inititalize tax2vec object adn fit and transform training data
-    tax2vec_instance = t2v.tax2vec(max_features=50, num_cpu=8, heuristic="pagerank", mode="index_word")
-    semantic_features_train = tax2vec_instance.fit_transform(train_x)
+        ## print rankings
+        for a, b in zip(tax2vec_instance.semantic_candidates, tax2vec_instance.pagerank_scores):
+            print("{} with score: {}".format(a, b))
 
-    ## print rankings
-    for a, b in zip(tax2vec_instance.semantic_candidates, tax2vec_instance.pagerank_scores):
-        print("{} with score: {}".format(a, b))
+        ## use tax2vec.WN to access hypernym graph
 
-    ## use tax2vec.WN to access hypernym graph
+if __name__ == '__main__':
+    run()
